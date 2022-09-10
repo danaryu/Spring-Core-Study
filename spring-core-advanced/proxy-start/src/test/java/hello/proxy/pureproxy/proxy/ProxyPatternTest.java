84609e77e69f106cc5d792a1482beda3a5643264
danaRyu <dana40426@gmail.com>
2022-09-10 18:27:10 +0900

    test: ProxyPattern Example

---
+++ spring-core-advanced/proxy-start/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.pureproxy.proxy;
+
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    /**
+     * client -> real subject
+     */
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    /**
+     * client -> cache proxy -> real subject
+     */
+    @Test
+    void cacheProxyTest() {
+        // Proxy 주입
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute(); // Proxy 호출에서 caching된 data 반환
+        client.execute();
+    }
+
+}
